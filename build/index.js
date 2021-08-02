431ad966f0daff2f79825807b9b956c5142c4353
luischo04 <buenrostroluisfernando04@gmail.com>
2021-08-02 00:03:49 -0500

    Configuración Básica
    Se ha hecho la instalación de los siguientes paquetes:
    * cors
    * express
    * morgan
    * promise-mysql
    * nodemon
    * concurrently
    
    Se ha configurado TypeScript para trabajar.
    
    Se ha creado la estructura de carpetas que se utilizará.
    
    Se ha creado archivo index.ts para la configuración básica de nuestro servidor.

---
+++ build/index.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
+class Server {
+    // Constructor de nuestro servidor
+    constructor() {
+        this.app = express_1.default();
+        this.config();
+        this.routes();
+    }
+    // Configuración del servidor
+    config() {
+        this.app.set("port", process.env.PORT || 3000);
+        this.app.use(morgan_1.default("dev"));
+        this.app.use(cors_1.default());
+        this.app.use(express_1.default.json());
+        this.app.use(express_1.default.urlencoded({ extended: false }));
+    }
+    // Rutas para mi APIRest
+    routes() { }
+    // Inicialización del servidor
+    start() {
+        this.app.listen(this.app.get('port'), () => {
+            console.log("Server on port", this.app.get('port'));
+        });
+    }
+}
+const server = new Server();
+server.start();
